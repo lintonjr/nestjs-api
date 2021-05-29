26eeef09a887e58f8156b69afea9e40823f02310
linton <linton@motorola.com>
2021-05-29 14:51:32 -0400

    chore: add support to swagger documentation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiBody } from '@nestjs/swagger';
+import { UserDto } from './dtos/user.dto';
 import { User } from './interfaces/user.interface';
 import { UsersService } from './users.service';
 
@@ -12,6 +14,7 @@ export class UsersController {
   }
 
   @Post()
+  @ApiBody({ type: UserDto })
   create(@Body() user: User): User {
     return this.usersService.create(user);
   }
